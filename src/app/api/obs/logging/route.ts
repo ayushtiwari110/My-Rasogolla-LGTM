35cf3d05a569a5da2fd23ab82a6180ab7a0866e0
Ayush Tiwari <ayushtiwari110@yahoo.com>
2025-03-30 20:12:35 +0530

    init LGTM

---
+++ src/app/api/obs/logging/route.ts
@@ -0,0 +1,25 @@
+export async function GET() {
+    try {
+      // throw new Error("Something went terribly wrong.")
+      globalThis?.logger?.info({
+        meta: {
+          requestId: crypto.randomUUID(),
+          extra: 'This is some extra information that you can add to the meta.',
+          anything: "anything"
+        },
+        message: 'Message of the log.',
+      });
+      return Response.json({
+        message: 'It works.',
+      });
+    } catch (error) {
+      globalThis?.logger?.error({
+        err: error,
+        message: 'Something went wrong.',
+      });
+      return Response.json({
+        message: 'An error occurred.',
+      }, { status: 500 });
+    }
+  }
+
